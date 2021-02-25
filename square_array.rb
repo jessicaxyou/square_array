e380806a3296bf17b035fa0fbb83c5b3b7d708f2
jessica you <jessicaxytan@gmail.com>
2021-02-25 16:45:27 -0500

    commit tree ea13f56b2992eccc6610cda33d9216df1faa4217

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  # array to call on each element
+  array.each do |number|
+    # sqaure the elements in an array
+    new_array<<(number * number)
+  end
+  new_array
 end
